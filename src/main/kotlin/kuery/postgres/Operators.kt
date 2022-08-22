a0615d7593aa0ddf8c52428cef9206675da826e4
Pablo Pallocchi <pablopallocchi@gmail.com>
2022-08-22 17:30:37 -0300

    add postgres contains operator

--- src/main/kotlin/kuery/postgres/Operators.kt
+++ src/main/kotlin/kuery/postgres/Operators.kt
@@ -8,3 +8,17 @@ import kuery.types.predicateOf
  * @sample kuery.postgres.OperatorsTest.match
  */
 infix fun String.match(value: Any) = predicateOf("$this @@ ?" , value)
+
+/**
+ * Returns a predicate where [this] @> {[value]}.
+ *
+ * @sample kuery.postgres.OperatorsTest.match
+ */
+infix fun String.contains(value: Any) = predicateOf("$this @> {?}" , value)
+
+/**
+ * Returns a predicate where NOT [this] @> {[value]}.
+ *
+ * @sample kuery.postgres.OperatorsTest.match
+ */
+infix fun String.notContains(value: Any) = predicateOf("NOT ($this @> {?})" , value)
--- src/test/kotlin/kuery/postgres/OperatorsKtTest.kt
+++ src/test/kotlin/kuery/postgres/OperatorsKtTest.kt
@@ -11,4 +11,18 @@ internal class OperatorsTest {
         Assertions.assertThat(predicate.toString()).isEqualTo("name @@ ?")
         Assertions.assertThat(predicate.args).containsExactly("ross")
     }
+
+    @Test
+    fun contains() {
+        val predicate = "name" contains "ross"
+        Assertions.assertThat(predicate.toString()).isEqualTo("name @> {?}")
+        Assertions.assertThat(predicate.args).containsExactly("ross")
+    }
+
+    @Test
+    fun notContains() {
+        val predicate = "name" notContains "ross"
+        Assertions.assertThat(predicate.toString()).isEqualTo("NOT (name @> {?})")
+        Assertions.assertThat(predicate.args).containsExactly("ross")
+    }
 }
